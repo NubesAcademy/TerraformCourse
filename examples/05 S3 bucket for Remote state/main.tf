7d81a11c11e05184905966fc3ef118416c655f13
Oleksiy Pototskyy <alexei.potocki@gmail.com>
2023-07-31 21:34:22 +0200

    Changes

---
+++ examples/05 S3 bucket for Remote state/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.73"
+    }
+  }
+
+  required_version = "1.3.6"
+  backend "s3" {
+    bucket = "s3-bucket-for-remote-state"
+    key    = "state.tfstate"
+    region = "eu-west-1"
+  }
+}
